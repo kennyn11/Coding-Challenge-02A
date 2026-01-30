3a07c0a591db2dbdffa4386f9d9e829fe08e274f
Kenny Nguyen <kennyn@usf.edu>
2026-01-30 16:23:15 -0500

    save changes to index.html

---
+++ pricing_assistant.js
@@ -0,0 +1,18 @@
+let productName = "White Tee"
+let costPerunit = 9.99
+let basePrice = 19.99
+let discountRate = 0.20
+let salesTaxRate = 0.07
+let fixedMonthlyCosts = 4000
+
+let discountedPrice = basePrice * (1 - discountRate)
+let finalPriceWithTax = discountedPrice * (1 + salesTaxRate)
+let profitPerUnit = finalPriceWithTax - costPerunit
+let breakEvenUnits = Math.ceil(fixedMonthlyCosts / profitPerUnit)
+let isProfitablePerUnit = profitPerUnit > 0
+
+console.log("White Tee:", productName)
+console.log("Discount before Tax:", discountedPrice)
+console.log("Total Cost:", basePrice)
+console.log("Break Even Units", breakEvenUnits)
+console.log("Calculated Profits:", profitPerUnit)
